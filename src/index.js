21af85f2988f4e7e4f551f4a1a3227aca53d1a38
Adrian Marimon <marimon.dev@Adrians-MacBook-Pro.local>
2021-07-01 23:57:39 -0400

    reorganized conde. Now class Game contain the states and is pass down to the rest of classes or fonctions by props

--- src/index.js
+++ src/index.js
@@ -4,50 +4,28 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={() => props.onClick()}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
   
 class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : "O"
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    })
-  }
-
   renderSquare(i) {
     return <Square 
-              value={this.state.squares[i]}
-              onClick={() => this.handleClick(i)}
+              value={this.props.squares[i]}
+              onClick={() => this.props.onClick(i)}
             />;
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares)
+    const winner = calculateWinner(this.props.squares)
     let status
 
     if(winner) {
       status = 'Winner' + winner
     } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+      status = `Next player: ${this.props.xIsNext ? 'X' : 'O'}`
     }
 
 
@@ -75,11 +53,37 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : "O"
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+  
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={this.state.squares}
+            xIsNext={this.state.xIsNext}
+            onClick={(i) => {this.handleClick(i)}}
+          />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
